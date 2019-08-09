7a5c373432a1de7d1e0ba36c771a7c44a0f09620
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2019-08-09 07:51:13 +0200

    Add exerice 1-6 solution

--- 1-6.scm
+++ 1-6.scm
@@ -1,20 +1,12 @@
-; procedures 
-(define (new-if predicate then-clause else-clause)
-        (cond 
-            (predicate then-clause)
-            (else else-clause)
-        )
-)
+(define (sqrt (x) 
+    (sqrt-iter 1.0 x)
+))
 
+;begins: sqr-iter with if and new if
 (define (sqrt-iter guess x)
     (if (good-enough? guess x)
         guess
         (sqrt-iter (improve guess x) x)))
-
-(sqrt-iter 1.0 x)
-
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
 
 (define (sqrt-iter guess x)
     (new-if (good-enough? guess x)
@@ -22,13 +14,63 @@
         (sqrt-iter (improve guess x) x)
     )
 )
+;ends: sqr-iter with if and new if
 
-;What happens when Alyssa attempts to use this to compute square roots? Explain.
+(define (new-if predicate then-clause else-clause)
+        (cond 
+            (predicate then-clause)
+            (else else-clause)
+        )
+)
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
 #|
-    [] order procedures above in order of execution
-    [] write down computation procedure for sqrt by using primitive if
-    [] write down computation procedure for sqrt by using new-if procedure
+    QUESTION
+    What happens when Alyssa attempts to use this to compute square roots? Explain.
+    
+    TODO
+    [x] write down computation procedure for sqrt by using primitive if
+    [x] write down computation procedure for sqrt by using new-if procedure
 |#
-(sqrt 2)
-; write down computation???
 
+; write down computation procedure for sqrt by using primitive if 
+(sqrt 2)
+(sqrt-iter 1.0 2)
+(if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x))
+(if (((< (abs (- (square guess) x)) 0.001)) guess x)
+    guess
+    (sqrt-iter (improve guess x) x))
+> guess ;hypothetically
+
+
+; write down computation procedure for sqrt by using new-if procedure
+(sqrt 2)
+
+(sqrt-iter 1.0 2)
+
+(new-if (good-enough? 1.0 2)
+        1.0
+        (sqrt-iter (1.0 2) 2))
+
+(new-if ((< (abs (- (square 1.0) 2)) 0.001))
+        1.0
+        (new-if (good-enough? 1.0 3)
+                1.0
+                (new-if (good-enough? 1.0 2)
+                        1.0
+                        (sqrt-iter (1.0 2) 2)))) ; and so on...
+
+#|
+    ANSWER
+    The scheme interpreter follows the applicative order which means that
+    - first it evaluates subprocedures and arguments 
+    - then when subprocedures are reduced to primitive it evaluates function calls to
+    Since new-if is a procedure itself, the interpreter proceeds to evaluate its subexpression.
+    One of new-if subprocedures is sqr-iter, which means it proceeds to evaluates it, 
+    which in turn calls new-if again and the process repeats.
+    And so on and so forth in a recursive loop
+|#
