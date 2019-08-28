96d548c26da365ba45cf337abea31e00f0da5420
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2019-08-28 08:01:38 +0200

    add notes to exercise 1-7

--- 1-7.scm
+++ 1-7.scm
@@ -2,11 +2,26 @@
     Notes:
     0. what is a mathematical square root function?
         -> A function of x such that y >= 0 and square of y = x
+
     0.5 how can we compute a square root?
         -> to compute the square root of a number we can use Newton's successive approximation
-    1. what does the good-enough? procedure do exactly?
+        that tries to find a sqrroot of a number by starting with a guess, 
+        and that continuosly improves that guess by averaging
+        - the sum of the radicand and the guess
+        - with the guess itself
+        ( ( radicand / guess ) + guess ) / 2 
+        sqr-iter does this.
+
+    1. what does the good-enough? procedure do exactly? This procedure tests wheter the square
+    of a guess is close enough to a radicand. To check this closeness it uses an arbitrary
+    mark (0.001)
+    In other words if square-of-guess - radicand < 0.001 then our guess is good enough 
+    to be considered the square root of said radicand
+
     2. why the test says that this procedure will not be effective 
-    for finding/evaluaing very small numbers?
+    for finding/evaluaing very small numbers? Don't know, need to check by compiling this
+    procedure 
+
     3. what does it mean that in real computers, arithmetic operations 
     are performed with limited precision. Thus good-enough? procedure 
     is inadequate for very large numbers?
